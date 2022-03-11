e9aecc92b59744cf8e4f105698bda178093d3b75
El Pacific Binagha <elpacificmb@gmail.com>
2022-03-10 17:39:27 -0800

    Advanced Tasks

---
+++ 0x00-hello_world/100-intel
@@ -0,0 +1,3 @@
+#!/bin/bash
+gcc -S -masm=intel $CFILE
+
---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Return:1
+ */
+int main(void)
+{
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, ch, sizeof(ch));
+return (1);
+}
