2e07bddb9b7156668f1c1a6e0bd82357eeafad9d
El Pacific Binagha <elpacificmb@gmail.com>
2022-03-10 18:15:14 -0800

    fix final task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
  */
 int main(void)
 {
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, ch, sizeof(ch));
+char ch[] = fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2,  ch, sizeof(ch));
 return (1);
 }
-
