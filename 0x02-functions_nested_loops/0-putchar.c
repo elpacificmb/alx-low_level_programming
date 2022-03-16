fea7263a91ca39f813a283d3896d11915f1a9bbd
El Pacific Binagha <elpacificmb@gmail.com>
2022-03-15 17:58:23 -0700

    Fix 0-putchar task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,19 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - Entry point
+ * Return:0
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
+int main(void)
+{
+char c[9] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
