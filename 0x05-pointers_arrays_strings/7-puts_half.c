64efbd80418a73d12ad7c44a7c2be95a38197d97
El Pacific Binagha <elpacificmb@gmail.com>
2022-03-21 15:58:52 -0700

    Fix task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,15 +7,27 @@
 
 void puts_half(char *str)
 {
-	int len = 0;
+	int len, n, i;
 
-	for (len = 0; str[len] != '\0'; len++)
-	;
+	len = 0;
 
-	for (int i = (len + 1) / 2; str[i] != '\0'; i++)
+	while (str[len] != '\0')
 	{
-		_putchar(str[i]);
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
 	}
 	_putchar('\n');
 }
-
