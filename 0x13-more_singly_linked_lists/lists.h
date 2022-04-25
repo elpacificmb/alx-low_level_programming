a798e52994933322ac965f43c92c63a8e887bb02
El Pacific Binagha <elpacificmb@gmail.com>
2022-04-25 10:39:58 -0700

    fix lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -34,10 +34,5 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-void free_listp(listp_t **head);
-size_t print_listint_safe(const listint_t *head);
-void free_listp2(listp_t **head);
-size_t free_listint_safe(listint_t **h);
-
 #endif
 
