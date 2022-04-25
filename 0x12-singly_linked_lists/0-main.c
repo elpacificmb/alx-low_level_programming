a296932e1960838d5daba3083dc65697c5f79b3a
El Pacific Binagha <elpacificmb@gmail.com>
2022-04-25 09:29:40 -0700

    Advanced tasks

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -2,19 +2,15 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
- */
-int main(void)
-{
+ */ int main(void) {
     list_t *head;
     list_t *new;
     list_t hello = {"World", 5, NULL};
     size_t n;
-
     head = &hello;
     new = malloc(sizeof(list_t));
     if (new == NULL)
@@ -28,13 +24,12 @@ int main(void)
     head = new;
     n = print_list(head);
     printf("-> %lu elements\n", n);
-
     printf("\n");
     free(new->str);
     new->str = NULL;
     n = print_list(head);
     printf("-> %lu elements\n", n);
-
     free(new);
     return (0);
 }
+
