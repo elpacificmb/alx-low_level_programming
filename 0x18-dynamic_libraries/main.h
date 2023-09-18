c9ca08328ffa647ba90a1f367bfd13b75b4d0fd3
Hafidovich007 <a.alahian@gmail.com>
2023-09-18 09:56:13 -0700

    Update7

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H_
-#define MAIN_H_
+#ifndef _main_h_
+#define _main_h_
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H_ */
+#endif /* _main_h_ */
